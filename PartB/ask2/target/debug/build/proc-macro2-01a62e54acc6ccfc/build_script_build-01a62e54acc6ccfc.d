f7ee4e2c17a583e51f18dabbff66ea3a356ee510
Mauragkas <51228949+Mauragkas@users.noreply.github.com>
2023-06-02 20:39:20 +0300

    Delete PartB/ask2/target directory

--- PartB/ask2/target/debug/build/proc-macro2-01a62e54acc6ccfc/build_script_build-01a62e54acc6ccfc.d
+++
@@ -1,5 +0,0 @@
-/home/mauragkas/Project/PartB/ask2/target/debug/build/proc-macro2-01a62e54acc6ccfc/build_script_build-01a62e54acc6ccfc: /home/mauragkas/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.59/build.rs
-
-/home/mauragkas/Project/PartB/ask2/target/debug/build/proc-macro2-01a62e54acc6ccfc/build_script_build-01a62e54acc6ccfc.d: /home/mauragkas/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.59/build.rs
-
-/home/mauragkas/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.59/build.rs:
--- PartB/ask2/target/debug/build/quote-2cdeb4f988fe155d/build_script_build-2cdeb4f988fe155d.d
+++
@@ -1,5 +0,0 @@
-/home/mauragkas/Project/PartB/ask2/target/debug/build/quote-2cdeb4f988fe155d/build_script_build-2cdeb4f988fe155d: /home/mauragkas/.cargo/registry/src/index.crates.io-6f17d22bba15001f/quote-1.0.28/build.rs
-
-/home/mauragkas/Project/PartB/ask2/target/debug/build/quote-2cdeb4f988fe155d/build_script_build-2cdeb4f988fe155d.d: /home/mauragkas/.cargo/registry/src/index.crates.io-6f17d22bba15001f/quote-1.0.28/build.rs
-
-/home/mauragkas/.cargo/registry/src/index.crates.io-6f17d22bba15001f/quote-1.0.28/build.rs:
