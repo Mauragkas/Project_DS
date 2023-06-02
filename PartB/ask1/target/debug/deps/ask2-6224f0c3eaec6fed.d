c6c4ec3fbdb4ec264e800246f5d5c82c576aa4eb
Mauragkas <51228949+Mauragkas@users.noreply.github.com>
2023-06-02 20:39:09 +0300

    Delete PartB/ask1/target directory

--- PartB/ask1/target/debug/deps/ask2-6224f0c3eaec6fed.d
+++
@@ -1,5 +0,0 @@
-/home/mauragkas/Project/PartB/ask1/target/debug/deps/ask2-6224f0c3eaec6fed.rmeta: src/main.rs
-
-/home/mauragkas/Project/PartB/ask1/target/debug/deps/ask2-6224f0c3eaec6fed.d: src/main.rs
-
-src/main.rs:
